76d8ea6e0ce2014aef694181d47fabd24f866f07
Rei Shinozuka <git@shinozuka.org>
2024-05-19 12:45:04 -0400

    added channel.sh example for channels

---
+++ channel.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+# runs cohorts by channel
+
+cohort=(F_VINT=2017 F_TERM=360 F_ELIG)
+../00util/run_sql_v24.sh fn_cohort_v01.sql "${cohort[@]}"
+
+for CHAN in R B C
+do
+    parms=("${cohort[@]}")
+    parms+=(F_CHAN=${CHAN})
+    parms+=(BY_AGE)
+    ../00util/run_sql_v24.sh fn_cohort_v01.sql "${parms[@]}"
+done
+
+greppattern="$(for i in "${cohort[@]}";do printf "%s%s" "$i" '.*';done )"
+files=($(ls _output/*.tsv  | grep "${greppattern}"))
+for file in "${files[@]}"
+do
+    head -2 ${file} | cut -f1-3 | sed "s^$^\t${file}^"
+done
+

95d2401bd025ca2e516f9e1279dd4d3e78f6e95a
Rei Shinozuka <git@shinozuka.org>
2024-05-28 10:40:15 -0400

    improved scripts slightly

--- channel.sh
+++ channel.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/env bash
 # runs cohorts by channel
 
+export QSCRIPT=fn_cohort_v02.sql 
 cohort=(F_VINT=2014 F_TERM=360 F_ELIG)
-../00util/run_sql_v24.sh fn_cohort_v01.sql "${cohort[@]}"
+# run the cohort summary
+../00util/run_sql_v24.sh ${QSCRIPT} "${cohort[@]}"
 
 for CHAN in R B C
 do
     parms=("${cohort[@]}")
-    parms+=(F_CHAN=${CHAN})
+    parms+=(F_CHAN=${CHAN} BY_CHAN)
     parms+=(BY_AGE)
-    ../00util/run_sql_v24.sh fn_cohort_v01.sql "${parms[@]}"
+    ../00util/run_sql_v24.sh ${QSCRIPT} "${parms[@]}"
 done
 
 greppattern="$(for i in "${cohort[@]}";do printf "%s%s" "$i" '.*';done )"
--- fn_loansize_v01.sh
+++ fn_loansize_v01.sh
@@ -3,6 +3,7 @@
 export cohort=(F_VINT=2014 F_ELIG UNIT=1)
 export loan_sizes=(150 200 250 500 9999)
 export oterms=(360)
+export QSCRIPT=fn_cohort_v02.sql
 #
 for oterm in "${oterms[@]}" ;
 do
@@ -14,7 +15,7 @@ do
 	parms+=(BALN=${loan_max})
 	parms+=(BAL0=${loan_min})
 	parms+=(BY_AGE)
-	../00util/run_sql_v24.sh fn_cohort_v01.sql  "${parms[@]}"
+	../00util/run_sql_v24.sh "${QSCRIPT}"  "${parms[@]}"
 	loan_min=${loan_max} # make sure loan size in innermost loop!!
     done # loan_max
 done # oterm
